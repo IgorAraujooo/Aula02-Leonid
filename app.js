dea8995a5bb2d53d5b32dc865007c029031c75bc
IGOR LUIS DE ANDRADE ARAUJO <23111733@CFP127ALUNO.LOCAL>
2023-08-09 11:51:04 -0300

    finalizado

---
+++ app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const forma1 = document.getElementById('forma1');
+const forma2 = document.getElementById('forma2');
+const forma3 = document.getElementById('forma3');
+const forma4 = document.getElementById('forma4')
+const forma5 = document.getElementById('forma5')
+const forma6 = document.getElementById('forma6')
+const forma7 = document.getElementById('forma7')
+
+function trocarCor() {
+    forma1.classList.toggle('amarelo');
+}
+
+function trocarForma() {
+    forma2.classList.toggle('circulo');
+}
+
+function trocarFormaCor() {
+    forma3.classList.toggle('amareloCirculo');
+}
+
+function girar(){
+    forma4.classList.toggle('giro')
+}
+
+function sumir (){
+    forma5.classList.toggle('sumir')
+}
+
+function mudarFundo () {
+    forma6.classList.toggle('fundo')
+}
+
+function mensagem(){
+    alert('eai leonid')
+}
+
+forma1.addEventListener('click', trocarCor);
+forma2.addEventListener('click', trocarForma);
+forma3.addEventListener('click', trocarFormaCor);
+forma4.addEventListener('click', girar)
+forma5.addEventListener('click', sumir)
+forma6.addEventListener('click', mudarFundo)
+forma7.addEventListener('click', mensagem )
